f92cca0f81ebf57dfdb6210ff30677053db3c69e
snowyunee <snowyunee@localhost.localdomain>
2015-06-05 13:57:46 +0900

    exercise 2.37-39

--- src/ch2.2/e2.33_39.hs
+++ src/ch2.2/e2.33_39.hs
@@ -49,20 +49,34 @@ accumulate_n op init l@((x:xs):xss) =
 accumulate_n op init _ = []
 
 
+-- exercise 2.37
 
---dot_product v w =
---  accumulate (+) 0 (mymap (*) v w)
---
---matrix_mul_vector m v =
---  map () m
---
---transpose mat
---  accumulate_n - - mat
---
---matrix_mul_matrix m n
---  let cols = transpose n
---  in map - m
+mymap2 :: (a -> b -> c) -> [a] -> [b] -> [c]
+mymap2 op xs ys = map (uncurry op) $ zip xs ys
+  
 
+dot_product v w =
+  accumulate (+) 0 (mymap2 (\a b -> a * b) v w)
+
+matrix_mul_vector m v =
+  map (\mi -> dot_product mi v) m
+
+transpose mat =
+  accumulate_n cons [] mat
+
+matrix_mul_matrix m n =
+  let cols = transpose n
+  in map (\ row -> matrix_mul_vector cols row) m
+
+-- exercise 2.38
+-- no source
+
+-- exercise 2.39
+reverse1 sequence =
+  foldr (\x acc -> acc ++ [x]) [] sequence
+
+reverse2 sequence =
+  foldl (\acc x -> x:acc) [] sequence
 
 main = do
   print $ mymap (* 2) [1,2,3]
@@ -74,5 +88,24 @@ main = do
   print $ count_leaves tree
   print $ "[22,26,30]"
   print $ accumulate_n (+) 0 [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
---  print $ dot_product [10,10,10] [[1,1,1],[2,2,2],[3,3,3]]
+  print $ "60"
+  print $ dot_product [10,10,10] [1,2,3]
+  print $ "[60,120,180]" 
+  print $ matrix_mul_vector [[10,10,10],[20,20,20],[30,30,30]] [1,2,3]
+  print $ "[10,20,30],[10,20,30],[10,20,30]]"
+  print $ transpose [[10,10,10],[20,20,20],[30,30,30]]
+  print $ "[[600,600,600],[1200,1200,1200],[1800,1800,1800]]" 
+  print $ matrix_mul_matrix [[10,10,10],[20,20,20],[30,30,30]] [[10,10,10],[20,20,20],[30,30,30]]
+  -- exercise 2.38
+  print $ foldr (/) 1 [1,2,3]
+  print $ foldl (/) 1 [1,2,3]
+  -- exercise 2.39
+  print $ "[3,2,1]"
+  print $ reverse1 [1,2,3]
+  print $ "[3,2,1]"
+  print $ reverse2 [1,2,3]
+
+
+
+
 
