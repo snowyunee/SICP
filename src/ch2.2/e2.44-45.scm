5a6c40ba355d43de4e3850f260f70aa341ba81db
snowyunee <snowyunee@localhost.localdomain>
2015-06-19 14:22:21 +0900

    sicp exercise

---
+++ src/ch2.2/e2.44-45.scm
@@ -0,0 +1,71 @@
+#lang scheme
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+ 
+ ;(define (right-split painter n)
+ ;  (if (= n 0)
+ ;      painter
+ ;      (let ((smaller (right-split painter (- n 1))))
+ ;        (beside painter (below smaller smaller)))))
+
+;(define (up-split painter n)
+;  (if (= n 0)
+;      painter
+;      (let ((smaller (up-split painter (- n 1))))
+;        (below painter (beside smaller smaller)))))
+
+(define (split f1 f2)
+  (lambda (painter n)
+      (if (= n 0)
+              painter
+                      (let ((smaller (split painter (- n 1))))
+                                (f1 painter (f2 smaller smaller))))))
+                                (define right-split (split beside below))
+                                (define up-split (split below beside))
+
+
+
+(define (corner-split painter n)
+  (if (= n 0)
+        painter
+              (let ((up (up-split painter (- n 1)))
+                          (right (right-split painter (- n 1))))
+                                  (let ((top-left (beside up up))
+                                                (bottom-right (below right right))
+                                                              (corder (corner-split painter (- n 1))))
+                                                                        (beside (below painter top-left)
+                                                                                          (below bottom-right corder))))))
+
+
+
+
+
+(define (square-of-four tl tr b1 br)
+  (lambda (painter)
+      (let ((top (beside (tl painter) (tr painter)))
+                (bottom (beside (b1 painter) (br painter))))
+                      (below bottom top))))
+
+;(define (flipped-pairs painter)
+;  (let ((painter2 (beside painter (flip-vert painter))))
+;    (below painter2 painter2)))
+
+(define (flipped-pairs painter)
+  (let ((combine4 (square-of-four identity flip-vert
+                                    identity flip-vert)))
+                                        (combine4 painter)))
+
+;(define (square-limit painter n)
+;  (let ((quarter (corner-split painter n)))
+;    (let ((half (beside (flip-horiz quarter) quarter)))
+;      (below (flip-vert half) half))))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                    rotate180 flip-vert)))
+                                        (combine4 (corner-split painter n))))
+
+
+
+
+
+
