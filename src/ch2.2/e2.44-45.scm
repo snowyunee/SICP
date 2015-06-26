0b634c6ad6cb7e2841e6a2c334effd3d6bd79be3
snowyunee <snowyunee@neowiz.com>
2015-06-26 13:56:02 +0900

    indentation

--- src/ch2.2/e2.44-45.scm
+++ src/ch2.2/e2.44-45.scm
@@ -1,12 +1,13 @@
 #lang scheme
 (require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
  
- ;(define (right-split painter n)
- ;  (if (= n 0)
- ;      painter
- ;      (let ((smaller (right-split painter (- n 1))))
- ;        (beside painter (below smaller smaller)))))
+;(define (right-split painter n)
+;  (if (= n 0)
+;      painter
+;      (let ((smaller (right-split painter (- n 1))))
+;        (beside painter (below smaller smaller)))))
 
+; exercise 2.44
 ;(define (up-split painter n)
 ;  (if (= n 0)
 ;      painter
@@ -15,25 +16,27 @@
 
 (define (split f1 f2)
   (lambda (painter n)
-      (if (= n 0)
-              painter
-                      (let ((smaller (split painter (- n 1))))
-                                (f1 painter (f2 smaller smaller))))))
-                                (define right-split (split beside below))
-                                (define up-split (split below beside))
+    (if (= n 0)
+        painter
+        (let ((smaller (split painter (- n 1))))
+          (f1 painter (f2 smaller smaller))))))
+
+
+(define right-split (split beside below))
+(define up-split (split below beside))
 
 
 
 (define (corner-split painter n)
   (if (= n 0)
-        painter
-              (let ((up (up-split painter (- n 1)))
-                          (right (right-split painter (- n 1))))
-                                  (let ((top-left (beside up up))
-                                                (bottom-right (below right right))
-                                                              (corder (corner-split painter (- n 1))))
-                                                                        (beside (below painter top-left)
-                                                                                          (below bottom-right corder))))))
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corder (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corder))))))
 
 
 
@@ -41,18 +44,19 @@
 
 (define (square-of-four tl tr b1 br)
   (lambda (painter)
-      (let ((top (beside (tl painter) (tr painter)))
-                (bottom (beside (b1 painter) (br painter))))
-                      (below bottom top))))
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (b1 painter) (br painter))))
+      (below bottom top))))
 
 ;(define (flipped-pairs painter)
 ;  (let ((painter2 (beside painter (flip-vert painter))))
 ;    (below painter2 painter2)))
 
+
 (define (flipped-pairs painter)
   (let ((combine4 (square-of-four identity flip-vert
-                                    identity flip-vert)))
-                                        (combine4 painter)))
+                                  identity flip-vert)))
+    (combine4 painter)))
 
 ;(define (square-limit painter n)
 ;  (let ((quarter (corner-split painter n)))
@@ -61,8 +65,8 @@
 
 (define (square-limit painter n)
   (let ((combine4 (square-of-four flip-horiz identity
-                                    rotate180 flip-vert)))
-                                        (combine4 (corner-split painter n))))
+                                  rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
 
 
 
