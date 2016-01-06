d6f410268096095cdf357eaf31eb94b9fad9f8de
snowyunee <snowyunee@gmail.com>
2016-01-06 15:44:22 +0900

    sicp e2.90 ing

--- src/ch2.3/e2.87.scm
+++ src/ch2.3/e2.87.scm
@@ -12,78 +12,105 @@
 (define (div x y) (apply-generic 'div x y))
 (define (=zero? x) (apply-generic 'zero? x))
 
-;############################################################
-;########## terms              ##############################
-(define (add-terms L1 L2)
-  (cond ((empty-termlist? L1) L2)
-        ((empty-termlist? L2) L1)
-        (else
-         (let ((t1 (first-term L1)) (t2 (first-term L2)))
-           (cond ((> (order t1) (order t2))
-                  (adjoin-term
-                   t1 (add-terms (rest-terms L1) L2)))
-                 ((< (order t1) (order t2))
-                  (adjoin-term
-                   t2 (add-terms L1 (rest-terms L2))))
-                 (else
-                  (adjoin-term
-                   (make-term (order t1)
-                              (add (coeff t1) (coeff t2)))
-                   (add-terms (rest-terms L1)
-                              (rest-terms L2)))))))))
+(define (make-sparse-terms terms) ((get 'make-terms 'sparse terms)))
+(define (make-dense-terms terms)  ((get 'make-terms 'dense-terms terms)))
+(define (add-terms x y) (apply-generic 'add-terms x y))
+(define (sub-terms x y) (apply-generic 'sub-terms x y))
+(define (mul-terms x y) (apply-generic 'mul-terms x y))
+(define (zero-terms? x) (apply-generic 'zero-terms? x))
 
-(define (sub-terms L1 L2)
-  (cond ((empty-termlist? L1) L2)
-        ((empty-termlist? L2) L1)
-        (else
-         (let ((t1 (first-term L1)) (t2 (first-term L2)))
-           (cond ((> (order t1) (order t2))
-                  (adjoin-term
-                   t1 (sub-terms (rest-terms L1) L2)))
-                 ((< (order t1) (order t2))
-                  (adjoin-term
-                   t2 (sub-terms L1 (rest-terms L2))))
-                 (else
-                  (adjoin-term
-                   (make-term (order t1)
-                              (sub (coeff t1) (coeff t2)))
-                   (add-terms (rest-terms L1)
-                              (rest-terms L2)))))))))
-
-(define (mul-terms L1 L2)
-  (if (empty-termlist? L1)
-      (the-empty-termlist)
-      (add-terms (mul-term-by-all-terms (first-term L1) L2)
-                 (mul-terms (rest-terms L1) L2))))
-(define (mul-term-by-all-terms t1 L)
-  (if (empty-termlist? L)
-      (the-empty-termlist)
-      (let ((t2 (first-term L)))
-        (adjoin-term
-         (make-term (+ (order t1) (order t2))
-                    (mul (coeff t1) (coeff t2)))
-         (mul-term-by-all-terms t1 (rest-terms L))))))
-
-(define (adjoin-term term term-list)
-  (if (=zero? (coeff term))
-      term-list
-      (cons term term-list)))
-(define (the-empty-termlist) '())
-(define (first-term term-list) (car term-list))
-(define (rest-terms term-list) (cdr term-list))
-(define (empty-termlist? term-list) (null? term-list))
-(define (make-term order coeff) (list order coeff))
-(define (order term) (car term))
-(define (coeff term) (cadr term))
-; e 2.87
-(define (zero-terms? L)
-  (cond ((empty-termlist? L) #t)
-        ((not (=zero? (coeff (first-term L)))) #f)
-        (else (zero-terms? rest-terms L))))
 
 ;############################################################
+;########## sparse terms              #######################
+(define (install-sparse-terms-package)
+  (define (add-sparse-terms L1 L2)
+    ;(println (list 'add-sparse-terms-L1-L2 'L1 L1 'L2 L2))
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1)) (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term
+                     t1 (add-sparse-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term
+                     t2 (add-sparse-terms L1 (rest-terms L2))))
+                   (else
+                    (adjoin-term
+                     (make-term (order t1)
+                                (add (coeff t1) (coeff t2)))
+                     (add-sparse-terms (rest-terms L1)
+                                (rest-terms L2)))))))))
+  
+  (define (sub-sparse-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1)) (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term
+                     t1 (sub-sparse-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term
+                     t2 (sub-sparse-terms L1 (rest-terms L2))))
+                   (else
+                    (adjoin-term
+                     (make-term (order t1)
+                                (sub (coeff t1) (coeff t2)))
+                     (sub-sparse-terms (rest-terms L1)
+                                (rest-terms L2)))))))))
+  
+  (define (mul-sparse-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-sparse-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-sparse-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term
+           (make-term (+ (order t1) (order t2))
+                      (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms t1 (rest-terms L))))))
+  
+  (define (adjoin-term term term-list)
+    (println (list 'adjoin-term 'term term 'term-list term-list))
+    (println (list 'coeff (coeff term)))
+    (if (=zero? (coeff term))
+        term-list
+        (cons term term-list)))
+  (define (the-empty-termlist) '())
+  (define (first-term term-list) (car term-list))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (empty-termlist? term-list) (null? term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+  ; e 2.87
+  (define (zero-sparse-terms? L)
+    (println (list (empty-termlist? L)) )
+    (println (list (=zero? (coeff (first-term L))) ))
+    (cond ((empty-termlist? L) #t)
+          ((not (=zero? (coeff (first-term L)))) #f)
+          (else (zero-sparse-terms? rest-terms L))))
+  
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'sparse p))
+  (put 'add-terms '(sparse sparse) 
+       (lambda (p1 p2) (tag (add-sparse-terms p1 p2))))
+  (put 'sub-terms '(sparse sparse) 
+       (lambda (p1 p2) (tag (sub-sparse-terms p1 p2))))
+  (put 'mul-terms '(sparse sparse) 
+       (lambda (p1 p2) (tag (mul-sparse-terms p1 p2))))
+  (put 'make-terms 'sparse
+       (lambda (terms) (tag terms)))
+  (put 'zero-terms? '(sparse)
+       (lambda (p) (tag (zero-sparse-terms? p))))
+  'done-sparse-terms)
+  
 ;############################################################
-
+;############################################################
 
 
 ;############################################################
@@ -114,6 +141,8 @@
   ;<procedures used by mul-poly>
   ; 책 다음 페이지에서 가져옴
   (define (add-poly p1 p2)
+    (println p1)
+    (println p2)
     (if (same-variable? (variable p1) (variable p2))
         (make-poly (variable p1)
                    (add-terms (term-list p1)
@@ -157,32 +186,48 @@
   ; e 2.88
   (put 'sub '(polynomial polynomial)
        (lambda (p1 p2) (tag (sub-poly p1 p2))))
-  'done)
+  'done-install-polynomial)
 ;############################################################
 ;############################################################
 
 (install-polynomial-package)
+(install-sparse-terms-package)
 
 (define (make-polynomial var terms)
   ((get 'make 'polynomial) var terms))
 
 
-(define p1 (make-polynomial 'x (adjoin-term (make-term 3 1) (adjoin-term (make-term 2 2) (adjoin-term (make-term 1 1) (the-empty-termlist))))))
-(define p2 (make-polynomial 'x (adjoin-term (make-term 3 3) (adjoin-term (make-term 2 3) (adjoin-term (make-term 1 3) (the-empty-termlist))))))
-(add p1 p2)
+(define p1 (make-polynomial 'x (cons 'sparse (list (list 3 1) (list 2 2) (list 1 1)))))
+(define p2 (make-polynomial 'x (cons 'sparse (list (list 3 3) (list 2 3) (list 1 3)))))
+;(add p1 p2)
 
 
 ; Exercise 2.87.  Install =zero? for polynomials in the generic arithmetic package. This will allow adjoin-term to work for polynomials with coefficients that are themselves polynomials.
 ; see commend e 2.87
 (println "2.87")
-(define pp1 (make-polynomial 'y (adjoin-term (make-term 3 p1) (adjoin-term (make-term 2 p1) (adjoin-term (make-term 1 1) (the-empty-termlist))))))
-(define pp2 (make-polynomial 'y (adjoin-term (make-term 3 p2) (adjoin-term (make-term 2 p2) (adjoin-term (make-term 1 3) (the-empty-termlist))))))
+(define pp1 (make-polynomial 'y (cons 'sparse (list (list 3 p1) (list 2 p1) (list 1 1)))))
+(define pp2 (make-polynomial 'y (cons 'sparse (list (list 3 p2) (list 2 p2) (list 1 3)))))
+(println pp1)
+(println pp2)
 (add pp1 pp2)
 
 ; Exercise 2.88.  Extend the polynomial system to include subtraction of polynomials. (Hint: You may find it helpful to define a generic negation operation.)
 (println "2.88")
 (sub p1 p2)
-
+(sub pp1 pp2)
 
 ; Exercise 2.89 Define procedures that implement the term-list representation described above as appropriate for dense polynomials.
+
+; Exercise 2.90.  Suppose we want to have a polynomial system that is efficient for both sparse and dense polynomials. One way to do this is to allow both kinds of term-list representations in our system. The situation is analogous to the complex-number example of section 2.4, where we allowed both rectangular and polar representations. To do this we must distinguish different types of term lists and make the operations on term lists generic. Redesign the polynomial system to implement this generalization. This is a major effort, not a local change.
+
+
+; Exercise 2.91.  A univariate polynomial can be divided by another one to produce a polynomial quotient and a polynomial remainder. For example,
+
+
+
+
+
+
+
+
 
